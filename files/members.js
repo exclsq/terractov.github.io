4b49271931fd35b9bf2a24110f014bb47750c4c8
exclsq <snfnormu@gmail.com>
2025-05-18 09:33:34 +0400

    Update members.js

--- files/members.js
+++ files/members.js
@@ -18,11 +18,7 @@ const profiles = {
   hardline: {
     img: 'https://i.pinimg.com/736x/cc/e6/4e/cce64e18c15c55e96844f8f280ec770d.jpg',
     desc: 'When Leo fucked, he didnt think twice',
-  },
-   imessd: {
-    img: 'https://i.pinimg.com/736x/cc/e6/4e/cce64e18c15c55e96844f8f280ec770d.jpg',
-    desc: 'When Leo fucked, he didnt think twice',
-   }
+  }
 
 };
 
